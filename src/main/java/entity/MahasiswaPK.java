bbf0f6f291c97df0c169303435a88c0b1408ecbd
dhabawidhi <wikodhawi@gmail.com>
2018-02-19 17:27:29 +0700

    second commit

---
+++ src/main/java/entity/MahasiswaPK.java
@@ -0,0 +1,24 @@
+package entity;
+
+import java.io.Serializable;
+
+public class MahasiswaPK implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * 
+	 */
+	
+	private String nim;
+	public String getNim() {
+		return nim;
+	}
+	public void setNim(String nim) {
+		this.nim = nim;
+	}
+	
+	
+}
---
+++ src/main/java/entity/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Leo Sendra
+ *
+ */
+package entity;
